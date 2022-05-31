84873c3ffa641b63dbb4f06c6de717b74d1f713c
Stiven Diaz <stiven@ozon.mobi>
2022-05-30 23:01:04 -0500

    instalacion, configuracion y conexión con mongoose

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,12 +2,29 @@ import { ConfigType } from '@nestjs/config';
 import { Module, Global } from '@nestjs/common';
 import { MongoClient } from 'mongodb';
 import config from '../config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
-
 @Global()
 @Module({
+  imports: [ 
+    // MongooseModule.forRoot('mongodb://localhost:27017', {
+    //   // user: 'root',
+    //   // pass: 'root',
+    //   dbName: 'platzi-store',
+    // }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { connection, host, port, dbName } = configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          dbName,
+        }
+      },
+      inject: [config.KEY]
+    })
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -28,6 +45,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY]
     }
   ],
-  exports: ['API_KEY', 'MONGO'],
+  exports: ['API_KEY', 'MONGO', MongooseModule],
 })
 export class DatabaseModule {}
